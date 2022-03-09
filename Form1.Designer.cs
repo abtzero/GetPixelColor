545890e64ce8cb880dd2438c65fdb825977493ae
Sven Rowoldt <abtzero@gmail.com>
2022-03-09 20:15:32 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace GetPixelColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.textBoxInterval = new System.Windows.Forms.TextBox();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.textR = new System.Windows.Forms.TextBox();
+            this.textG = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.txtBoxPause = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(433, 213);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 210);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 325);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 95);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "TEST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxX.Location = new System.Drawing.Point(16, 15);
+            this.textBoxX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxX.Size = new System.Drawing.Size(152, 68);
+            this.textBoxX.TabIndex = 2;
+            this.textBoxX.Text = "131";
+            this.textBoxX.TextChanged += new System.EventHandler(this.textBoxX_TextChanged);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxY.Location = new System.Drawing.Point(16, 91);
+            this.textBoxY.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxY.Size = new System.Drawing.Size(152, 68);
+            this.textBoxY.TabIndex = 3;
+            this.textBoxY.Text = "39";
+            this.textBoxY.TextChanged += new System.EventHandler(this.textBoxY_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(196, 28);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Maximum = 1920;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(628, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 131;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(196, 91);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar2.Maximum = 1080;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(628, 56);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 39;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAuto.Location = new System.Drawing.Point(16, 213);
+            this.btnAuto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(225, 95);
+            this.btnAuto.TabIndex = 6;
+            this.btnAuto.Text = "AUTO";
+            this.btnAuto.UseVisualStyleBackColor = false;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // textBoxInterval
+            // 
+            this.textBoxInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInterval.Location = new System.Drawing.Point(249, 213);
+            this.textBoxInterval.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxInterval.Name = "textBoxInterval";
+            this.textBoxInterval.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxInterval.Size = new System.Drawing.Size(175, 98);
+            this.textBoxInterval.TabIndex = 7;
+            this.textBoxInterval.Text = "1";
+            this.textBoxInterval.TextChanged += new System.EventHandler(this.textBoxInterval_TextChanged);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(196, 150);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar3.Maximum = 250;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(628, 56);
+            this.trackBar3.TabIndex = 8;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // textR
+            // 
+            this.textR.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textR.Location = new System.Drawing.Point(16, 442);
+            this.textR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textR.Name = "textR";
+            this.textR.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textR.Size = new System.Drawing.Size(235, 128);
+            this.textR.TabIndex = 9;
+            this.textR.Text = "0";
+            // 
+            // textG
+            // 
+            this.textG.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textG.Location = new System.Drawing.Point(297, 442);
+            this.textG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textG.Name = "textG";
+            this.textG.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textG.Size = new System.Drawing.Size(235, 128);
+            this.textG.TabIndex = 10;
+            this.textG.Text = "0";
+            // 
+            // textB
+            // 
+            this.textB.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textB.Location = new System.Drawing.Point(588, 442);
+            this.textB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textB.Name = "textB";
+            this.textB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textB.Size = new System.Drawing.Size(235, 128);
+            this.textB.TabIndex = 11;
+            this.textB.Text = "0";
+            // 
+            // txtBoxPause
+            // 
+            this.txtBoxPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPause.Location = new System.Drawing.Point(249, 325);
+            this.txtBoxPause.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxPause.Name = "txtBoxPause";
+            this.txtBoxPause.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtBoxPause.Size = new System.Drawing.Size(175, 83);
+            this.txtBoxPause.TabIndex = 12;
+            this.txtBoxPause.Text = "4100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 587);
+            this.Controls.Add(this.txtBoxPause);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textG);
+            this.Controls.Add(this.textR);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.textBoxInterval);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.TextBox textBoxInterval;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TextBox textR;
+        private System.Windows.Forms.TextBox textG;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox txtBoxPause;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;
+
+
+//using WindowsInput;//For Input Simulator
+//using InputManager; //For Input Manager
+
+//using WindowsInput;
+//using WindowsInput.Native;
+
+//using Input
+
+
+// GetPixelMethof from: https://stackoverflow.com/questions/1483928/how-to-read-the-color-of-a-screen-pixel
+// mit Kommentar: "I think this is the cleanest and quickest way."
+
+namespace GetPixelColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool Auto = false;
+        private Color lastColor;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Color mycolor = Color.FromArgb(255, 0, 150, 255);
+            int X = int.Parse(textBoxX.Text);
+            int Y = int.Parse(textBoxY.Text);
+            //Color mycolor = GetColorAt(100, 50);
+            Color mycolor = GetColorAt(X, Y);
+            panel1.BackColor = mycolor;
+
+
+            System.Threading.Thread.Sleep(int.Parse(txtBoxPause.Text));
+            ///InputSimulator .SimulateKeyPress(VirtualKeyCode.VK_E);
+            //SendKeys.SendWait("e");
+            // https://archive.codeplex.com/?p=inputsimulator
+            //InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_E);
+            //var sim = new InputSimulator();
+            //sim.Keyboard.KeyPress(VirtualKeyCode.VK_E);
+     
+            short key = 0x12; // E key https://community.bistudio.com/wiki/DIK_KeyCodes
+            //WindowsMessageService.SendKey(key, KeyFlag.KeyDown);
+            //System.Threading.Thread.Sleep(20);
+            //WindowsMessageService.SendKey(key, KeyFlag.KeyUp);
+            
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBoxX.Text = trackBar1.Value.ToString();
+            //int X = int.Parse(textBoxX.Text);
+            //int Y = int.Parse(textBoxY.Text);
+            panel1.BackColor = GetColorAt(trackBar1.Value, trackBar2.Value);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            textBoxY.Text = trackBar2.Value.ToString();
+            //int X = int.Parse(textBoxX.Text);
+            //int Y = int.Parse(textBoxY.Text);
+            panel1.BackColor = GetColorAt(trackBar1.Value, trackBar2.Value);
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("AUTO: " + Auto);
+            if (Auto)
+            {
+                btnAuto.BackColor = Color.FromArgb(255, 244, 244, 244);
+                Auto = false;
+                timer1.Enabled = false;
+            }
+            else
+            {
+                btnAuto.BackColor = Color.FromArgb(255, 0, 200, 70);
+                Auto = true;
+                timer1.Enabled = true;
+            }
+        }
+
+        /*
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+        */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //panel1.BackColor = GetColorAt(trackBar1.Value, trackBar2.Value);
+            Color actColor = GetColorAt(int.Parse(textBoxX.Text), int.Parse(textBoxY.Text));
+            if (actColor != lastColor)
+            {
+                //panel1.BackColor = GetColorAt(int.Parse(textBoxX.Text), int.Parse(textBoxY.Text));
+                panel1.BackColor = actColor;
+                lastColor = actColor;
+                textR.Text = actColor.R.ToString();
+                textG.Text = actColor.G.ToString();
+                textB.Text = actColor.B.ToString();
+                Console.WriteLine(actColor.R.ToString() + " " + actColor.G.ToString() + " " + actColor.B.ToString());
+                //if(actColor.R >= 140 & actColor.R<= 170 & actColor.G >= 140 & actColor.G <= 170 & actColor.B >= 140 & actColor.B <= 170)
+                if (actColor.R == 229 & actColor.G == 229 & actColor.B == 229)
+                {
+                    this.BackColor = Color.Red;
+                    Console.WriteLine("===================     HIT COLOR!     ==================");
+                    timer1.Enabled = false;
+                    btnAuto.BackColor = Color.FromArgb(255, 244, 244, 244);
+                    Auto = false;
+                    this.Refresh();
+                    //this.BackColor = Color.Yellow;
+
+                    DoIt();
+                }
+                else
+                {
+                    this.BackColor = Color.FromArgb(255, 244, 244, 244);
+                }
+            }
+
+        }
+
+        private void DoIt() {
+            System.Threading.Thread.Sleep(int.Parse(txtBoxPause.Text));
+
+            this.BackColor = Color.Yellow;
+            this.Refresh();
+            //Application.DoEvents();
+
+            //SendKeys.SendWait("e");
+            //var sim = new InputSimulator();
+            //sim.Keyboard.KeyPress(VirtualKeyCode.VK_E);
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                short key = 0x1F; // x12 E key   // S: 0x1F x14 T https://community.bistudio.com/wiki/DIK_KeyCodes
+                Send_Key(key, 0x0000);
+                System.Threading.Thread.Sleep(20);
+                Console.WriteLine("- - - - - - - - - - -  SENT - - - - - - - - - - - - - -");
+                Send_Key(key, 0x0002);
+
+
+            }).Start();
+
+            this.Refresh();
+            this.BackColor = Color.FromArgb(255, 244, 244, 244);
+
+            System.Threading.Thread.Sleep(int.Parse(txtBoxPause.Text));
+
+            timer1.Enabled = true;
+            btnAuto.BackColor = Color.FromArgb(255, 0, 200, 70); // green
+            Auto = true;
+        }
+
+
+        private void Send(short key)
+        {
+            Send_Key(key, 0x0000);
+            System.Threading.Thread.Sleep(20);
+            Send_Key(key, 0x0002);
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar3.Value == 0) { trackBar3.Value = 1; }
+            textBoxInterval.Text = trackBar3.Value.ToString();
+            timer1.Interval = trackBar3.Value;
+        }
+
+        private void textBoxInterval_TextChanged(object sender, EventArgs e)
+        {
+            trackBar3.Value = int.Parse(textBoxInterval.Text);
+        }
+
+        private void textBoxX_TextChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = int.Parse(textBoxX.Text);
+        }
+
+        private void textBoxY_TextChanged(object sender, EventArgs e)
+        {
+            trackBar2.Value = int.Parse(textBoxY.Text);
+        }
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // https://stackoverflow.com/questions/28854034/simulate-keyboard-click-in-gtasa 
+        // https://www.codeproject.com/Questions/279641/c-sharp-DirectInput-Send-Key
+
+
+        [DllImport("user32.dll")]
+        static extern UInt32 SendInput(UInt32 nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+
+        const int KEYEVENTF_EXTENDEDKEY = 0x0001;
+        const int KEYEVENTF_KEYUP = 0x0002;
+        const int KEYEVENTF_UNICODE = 0x0004;
+        const int KEYEVENTF_SCANCODE = 0x0008;
+
+
+        public void Send_Key(short Keycode, int KeyUporDown)
+        {
+            INPUT[] InputData = new INPUT[1];
+
+            InputData[0].type = 1;
+            InputData[0].ki.wScan = Keycode;
+            InputData[0].ki.dwFlags = KeyUporDown;
+            InputData[0].ki.time = 0;
+            InputData[0].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(1, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+    }
+    
+}
